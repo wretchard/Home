ab6d366ca552061d09d536aa5d45794d0af34adf
Your_Wakanda_Developer_Name_Here <WakandaDeveloper@wakanda.org>
2015-02-23 04:12:14 +0000

    UIChanges #3

--- Wretchard/WebFolder/index.waPage/scripts/index-tablet.js
+++ Wretchard/WebFolder/index.waPage/scripts/index-tablet.js
@@ -2,38 +2,38 @@
 WAF.onAfterInit = function onAfterInit() {// @lock
 
 // @region namespaceDeclaration// @startlock
-	var menuItemBelmont = {};	// @menuItem
-	var menuItemAmzon = {};	// @menuItem
-	var menuItemTip = {};	// @menuItem
-	var menuItemHome = {};	// @menuItem
+	var menuItem4 = {};	// @menuItem
+	var menuItem3 = {};	// @menuItem
+	var menuItem2 = {};	// @menuItem
+	var menuItem1 = {};	// @menuItem
 // @endregion// @endlock
 
 // eventHandlers// @lock
 
-	menuItemBelmont.click = function menuItemBelmont_click (event)// @startlock
-	{// @endlock
-		window.open("http://pjmedia.com/richardfernandez/", "_blank")
-	};// @lock
-
-	menuItemAmzon.click = function menuItemAmzon_click (event)// @startlock
-	{// @endlock
-		window.open("https://www.amazon.com/author/fernandezrichard", "_blank")
-	};// @lock
-
-	menuItemTip.click = function menuItemTip_click (event)// @startlock
+	menuItem4.click = function menuItem4_click (event)// @startlock
 	{// @endlock
 		window.location.href='/tipjar.html'
 	};// @lock
 
-	menuItemHome.click = function menuItemHome_click (event)// @startlock
+	menuItem3.click = function menuItem3_click (event)// @startlock
+	{// @endlock
+		window.open("https://www.amazon.com/author/fernandezrichard", "_blank")
+	};// @lock
+
+	menuItem2.click = function menuItem2_click (event)// @startlock
+	{// @endlock
+		window.open("http://pjmedia.com/richardfernandez/", "_blank")
+	};// @lock
+
+	menuItem1.click = function menuItem1_click (event)// @startlock
 	{// @endlock
 		window.location.href='/'
 	};// @lock
 
 // @region eventManager// @startlock
-	WAF.addListener("menuItemBelmont", "click", menuItemBelmont.click, "WAF");
-	WAF.addListener("menuItemAmzon", "click", menuItemAmzon.click, "WAF");
-	WAF.addListener("menuItemTip", "click", menuItemTip.click, "WAF");
-	WAF.addListener("menuItemHome", "click", menuItemHome.click, "WAF");
+	WAF.addListener("menuItem4", "click", menuItem4.click, "WAF");
+	WAF.addListener("menuItem3", "click", menuItem3.click, "WAF");
+	WAF.addListener("menuItem2", "click", menuItem2.click, "WAF");
+	WAF.addListener("menuItem1", "click", menuItem1.click, "WAF");
 // @endregion
 };// @endlock
