ef0e6a81bf20080a266c29b77191d3cc375b8f6d
Your_Wakanda_Developer_Name_Here <WakandaDeveloper@wakanda.org>
2015-03-01 19:40:03 +1100

    Update #6

---
+++ Mobile/WebFolder/index.waPage/scripts/index.js
@@ -0,0 +1,18 @@
+﻿
+WAF.onAfterInit = function onAfterInit() {// @lock
+
+// @region namespaceDeclaration// @startlock
+	var dataGrid1 = {};	// @dataGrid
+// @endregion// @endlock
+
+// eventHandlers// @lock
+
+	dataGrid1.onRowClick = function dataGrid1_onRowClick (event)// @startlock
+	{// @endlock
+		
+	};// @lock
+
+// @region eventManager// @startlock
+	WAF.addListener("dataGrid1", "onRowClick", dataGrid1.onRowClick, "WAF");
+// @endregion
+};// @endlock
